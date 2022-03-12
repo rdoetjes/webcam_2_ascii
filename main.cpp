9cf65078f2fb9ec99310d8fb06c2e84cceadb044
Raymond Doetjes <rdoetjes@phonax.com>
2022-03-12 14:44:39 +0100

    remarks

--- main.cpp
+++ main.cpp
@@ -4,23 +4,56 @@
 
 using namespace cv;
 
+/*
+Sets the web came to the closest native resolution
+
+cap is video captur
+width is the width of the captured image
+height is the height of the captured image
+*/
 void setResolutionCam(VideoCapture *cap, const uint width, const uint height)
 {
   cap->set(CAP_PROP_FRAME_WIDTH, width);
   cap->set(CAP_PROP_FRAME_HEIGHT, height);
 }
 
-void imageDesatAndResize(Mat *input, Mat *output, const int width, const int height)
+/*
+Desaturares and resizes the image.
+
+input is the income image you wish to turn in to black and white and resize
+output contains the resized and desaturated images
+width is the width that output image should become
+height is the height that the output image should become
+*/
+void imageDesatAndResize(Mat *input, Mat *output, const uint width, const uint height)
 {
   cvtColor(*input, *output, COLOR_BGR2GRAY);
   cv::resize(*output, *output, Size(width, height), INTER_LINEAR);
 }
 
+/*
+maps the value x, in a range between out_min and out_max in relation to in_min and in_max
+
+x is the number to remap
+in_min is the minimal number that x can be
+in_max is the maximum number that x can be
+out_min is the minimum number that the result can become
+out_max is the maximum number that the result can become
+
+returns a long that is mapped between out_min and out_max
+*/
 long map(int x, int in_min, int in_max, int out_min, int out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+/*
+turnImageToAscii will look at each pixel value and find based on it's value the appropriate char from chars array.
+These chars in the chars array, simulate the luminance.
+Each pixel maps to a single character. So when you have 80x24 char screen the input images should be 80x24 pixels.
+
+input is the rescaled, grey scale image to be mapped in ascii chars
+*/
 void turnImageToAscii(Mat *input)
 {
   int cn = input->channels();
@@ -36,6 +69,9 @@ void turnImageToAscii(Mat *input)
   }
 }
 
+/*
+just a convenience wrapper for the thread
+*/
 void t_resize(Mat *image, const int width, const int height)
 {
   cv::resize(*image, *image, Size(width, height), INTER_LINEAR);
@@ -60,14 +96,21 @@ int main()
   while (running)
   {
     cap >> image;
+    //turns the image into a 140, 51 pixel black and white image, that will map to 140x51 chars
     imageDesatAndResize(&image, &small, 140, 51);
 
+    //resize the life feed to 320x240 to display, and capture keyboard events
     std::thread thread1(t_resize, &image, 320, 240);
+
+    //takes the small black and white image and maps each pixel to a character and prints it to the terminal
     std::thread thread2(turnImageToAscii, &small);
+
     thread1.join();
     thread2.join();
 
+    //show the web cam feed
     cv::imshow("RAW", image);
+
     if (waitKey(41) >= 0)
       running = false;
   }
