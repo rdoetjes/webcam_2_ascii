1930681bb6eda7531c8253dfe36f56adcd936b00
Raymond Doetjes <rdoetjes@phonax.com>
2022-03-13 19:59:55 +0100

    render to png

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 #include <thread>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <Magick++.h>
+#include <string.h>
 
 using namespace cv;
+using namespace Magick;
 
-#define TERM_WIDTH 140
-#define TERM_HEIGTH 51
+#define TERM_WIDTH 280
+#define TERM_HEIGTH 101
 #define MSWAIT 5
 
 /*
@@ -56,6 +59,12 @@ long map(int x, int in_min, int in_max, int out_min, int out_max)
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+void padTo(std::string &str, const size_t num, const char paddingChar = ' ')
+{
+    if(num > str.size())
+        str.insert(0, num - str.size(), paddingChar);
+}
+
 /*
 turnImageToAscii will look at each pixel value and find based on it's value the appropriate char from chars array.
 These chars in the chars array, simulate the luminance.
@@ -63,22 +72,39 @@ Each pixel maps to a single character. So when you have 80x24 char screen the in
 
 input is the rescaled, grey scale image to be mapped in ascii chars
 */
-void turnImageToAscii(Mat *input)
+void turnImageToAscii(Mat *input, const uint fps_count)
 {
   if (input == NULL)
     return;
 
   int cn = input->channels();
-  static char chars[] = "Æ@#W$9876543210?!abc;:+=-,._  ";
+  static char chars[] = "Æ@#W$9876543210?!abc;:+=-,._   ";
 
-  std::cout << "\033[1;1H\033[1;32m";
+  InitializeMagick(nullptr);
+  Image image( "1920x1080", "black" );
+  image.font("Courier");
+  image.fillColor(Color("GREEN"));
+  image.strokeColor(Color("GREEN"));
+  image.fontPointsize(10); 
+
+  std::list<Drawable> text_draw_list;
+  
+  std::string s = "";
+  std::string sfcount = std::to_string(fps_count);
+  padTo(sfcount, 4, '0');
+  std::string p = "./render/" +  sfcount + ".png";
+
   for (int i = 0; i < input->rows; i++)
   {
-    std::string s = "";
-    for (int j = 0; j < input->cols; j++)
+    for (int j = 0; j < input->cols; j++){
       s += chars[map(input->at<uint8_t>(i, j), 255, 0, 0, strlen(chars) - 1)];
-    std::cout << s << std::endl;
+    }
+    s += "\r\n";
   }
+
+  text_draw_list.push_back( DrawableText(0, 0, s));
+  image.draw(text_draw_list);
+  image.write( p ); 
 }
 
 /*
@@ -128,7 +154,7 @@ int main(int argc, char **argv)
 
   std::cout << "Set your terminal to " << TERM_WIDTH << "x" << TERM_HEIGTH << std::endl;
   std::cin.get();
-
+  static uint fps_count = 0;
   while (running)
   {
     cap >> image;
@@ -142,16 +168,19 @@ int main(int argc, char **argv)
     std::thread thread1(t_resize, &image, 320, 240);
 
     // takes the small black and white image and maps each pixel to a character and prints it to the terminal
-    std::thread thread2(turnImageToAscii, &small);
+    std::thread thread2(turnImageToAscii, &small, fps_count);
 
     thread1.join();
-    thread2.join();
-
+    
     // show the web cam feed
     cv::imshow("RAW", image);
 
-		if (waitKey(MSWAIT) >= 0)
+    thread2.join();
+
+		if (waitKey(1) >= 0)
             break;
+
+    fps_count++;
   }
 
   return 0;
